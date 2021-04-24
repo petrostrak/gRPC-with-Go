603c28fb4e6b86d29164520808b3c510d983e26e
petrostrak <pit.trak@gmail.com>
2021-04-24 22:53:43 +0300

    test protobuf client

---
+++ service/laptop_client_test.go
@@ -0,0 +1,73 @@
+package service_test
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"github.com/petrostrak/gRPC-with-Go/pb"
+	"github.com/petrostrak/gRPC-with-Go/sample"
+	"github.com/petrostrak/gRPC-with-Go/serializer"
+	"github.com/petrostrak/gRPC-with-Go/service"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestClientCreateLaptop(t *testing.T) {
+	t.Parallel()
+
+	laptopServer, serverAddress := startTestLaptopServer(t)
+	laptopClient := newTestLaptopClient(t, serverAddress)
+
+	laptop := sample.NewLaptop()
+	expectedId := laptop.Id
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Equal(t, expectedId, res.Id)
+
+	// check that the laptop is saved to the store
+	other, err := laptopServer.Store.Find(res.Id)
+	if err != nil {
+		require.NoError(t, err)
+		require.NotNil(t, other)
+	}
+
+	// check that the saved laptop is the same as the one we send
+	requireSameLaptop(t, laptop, other)
+}
+
+func requireSameLaptop(t *testing.T, laptop1, laptop2 *pb.Laptop) {
+	json1, err := serializer.ProtobufToJSON(laptop1)
+	require.NoError(t, err)
+
+	json2, err := serializer.ProtobufToJSON(laptop2)
+	require.NoError(t, err)
+
+	require.Equal(t, json1, json2)
+}
+
+func newTestLaptopClient(t *testing.T, serverAddress string) pb.LaptopServiceClient {
+	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure())
+	require.NoError(t, err)
+
+	return pb.NewLaptopServiceClient(conn)
+}
+
+func startTestLaptopServer(t *testing.T) (*service.LaptopServer, string) {
+	laptopServer := service.NewLaptopServer(service.NewInMemoryLaptopStore())
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	listener, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	grpc.Server(listener)
+
+	return laptopServer, listener.Addr().String()
+}
--- service/laptop_store.go
+++ service/laptop_store.go
@@ -15,6 +15,7 @@ var (
 
 type LaptopStore interface {
 	Save(*pb.Laptop) error
+	Find(string) (*pb.Laptop, error)
 }
 
 type InMemoryLaptopStore struct {
@@ -44,4 +45,22 @@ func (s *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
 
 	s.data[other.Id] = other
 	return nil
+}
+
+func (s *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	laptop := s.data[id]
+	if laptop == nil {
+		return nil, nil
+	}
+
+	// deep cory
+	other := &pb.Laptop{}
+	if err := copier.Copy(other, laptop); err != nil {
+		return nil, fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+
+	return other, nil
 }
