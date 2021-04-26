9de42bf8b253c94724c1b23513206ccf22f1f12e
petrostrak <pit.trak@gmail.com>
2021-04-26 18:39:54 +0300

    add user and user-store

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/google/uuid v1.2.0
 	github.com/jinzhu/copier v0.3.0
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
 	google.golang.org/grpc v1.37.0
 	google.golang.org/protobuf v1.26.0
 )
---
+++ service/user_store.go
@@ -0,0 +1,46 @@
+package service
+
+import "sync"
+
+type UserStore interface {
+	Save(*User) error
+	Fin(string) (*User, error)
+}
+
+// InMemoryUserStore stores users in memory
+type InMemoryUserStore struct {
+	mutex sync.RWMutex
+	users map[string]*User
+}
+
+// NewInMemoryUserStore returns a new in-memory user store
+func NewInMemoryUserStore() *InMemoryUserStore {
+	return &InMemoryUserStore{
+		users: make(map[string]*User),
+	}
+}
+
+// Save saves a user to the store
+func (store *InMemoryUserStore) Save(user *User) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.users[user.Username] != nil {
+		return ErrAlreadyExists
+	}
+
+	store.users[user.Username] = user.Clone()
+	return nil
+}
+
+func (store *InMemoryUserStore) Find(username string) (*User, error) {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	user := store.users[username]
+	if user == nil {
+		return nil, nil
+	}
+
+	return user.Clone(), nil
+}
